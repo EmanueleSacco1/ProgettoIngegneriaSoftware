c3d25159c4b6c4d4ff4fc226365433fdd520cec2
AlexandreAn <alemega83@gmail.com>
2024-05-03 20:02:25 +0200

    Update vistahome.py

--- ProgettoSoftwareGestionale/Home/vistahome.py
+++ ProgettoSoftwareGestionale/Home/vistahome.py
@@ -1,10 +1,51 @@
-from PyQt6.QtWidgets import QApplication, QWidget
-import sys
+from PyQt6 import QtCore, QtGui, QtWidgets
 
-app = QApplication(sys.argv)
 
-finestra = QWidget()
-finestra.setWindowTitle("Accesso area riservata")
-finestra.show()
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(767, 428)
+        self.btnaccedi = QtWidgets.QPushButton(parent=Form)
+        self.btnaccedi.setGeometry(QtCore.QRect(190, 260, 161, 28))
+        self.btnaccedi.setObjectName("btnaccedi")
+        self.pushButton = QtWidgets.QPushButton(parent=Form)
+        self.pushButton.setGeometry(QtCore.QRect(410, 260, 161, 28))
+        self.pushButton.setObjectName("pushButton")
+        self.lineEdit = QtWidgets.QLineEdit(parent=Form)
+        self.lineEdit.setGeometry(QtCore.QRect(360, 100, 211, 22))
+        self.lineEdit.setText("")
+        self.lineEdit.setObjectName("lineEdit")
+        self.label = QtWidgets.QLabel(parent=Form)
+        self.label.setGeometry(QtCore.QRect(300, 40, 291, 16))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(parent=Form)
+        self.label_2.setGeometry(QtCore.QRect(240, 100, 55, 16))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(parent=Form)
+        self.label_3.setGeometry(QtCore.QRect(230, 170, 55, 16))
+        self.label_3.setObjectName("label_3")
+        self.lineEdit_2 = QtWidgets.QLineEdit(parent=Form)
+        self.lineEdit_2.setGeometry(QtCore.QRect(360, 160, 211, 22))
+        self.lineEdit_2.setObjectName("lineEdit_2")
 
-sys.exit(app.exec())
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.btnaccedi.setText(_translate("Form", "Accedi"))
+        self.pushButton.setText(_translate("Form", "Cancella"))
+        self.label.setText(_translate("Form", "ACCEDI ALL\'AERA RISERVATA"))
+        self.label_2.setText(_translate("Form", "Email:"))
+        self.label_3.setText(_translate("Form", "Password:"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec())
