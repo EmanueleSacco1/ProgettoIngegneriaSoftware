d36cbbf05894283c24995deca3e1670f16198b1e
Emanuele Sacco <emanuele.sacco14@gmail.com>
2024-05-03 17:59:35 +0200

    Aggiunta

--- ProgettoSoftwareGestionale/Main/main.py
+++ ProgettoSoftwareGestionale/Main/main.py
@@ -1,6 +1,11 @@
-print "ciao a tutti"
-a=10
-b=2
-c=a+b;
-print c
-mannaggia a maometto
+import sys
+
+from PyQt6.QtWidgets import QApplication
+
+from Home.vistahome import vistahome
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    vista_home = vistahome()
+    vista_home.show()
+    sys.exit(app.exec())
