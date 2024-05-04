7ccec9daaaf07d058ac0fa55e39d029c249ec2a8
AlexandreAn <alemega83@gmail.com>
2024-05-04 09:23:35 +0200

    Creazione Area riservata per l'associato

---
+++ ProgettoSoftwareGestionale/AreaAssociato/areariservataassociato.py
@@ -0,0 +1,58 @@
+from PyQt6 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("AreaRiservataAssociato")
+        Form.resize(890, 562)
+        self.label = QtWidgets.QLabel(parent=Form)
+        self.label.setGeometry(QtCore.QRect(480, 40, 55, 16))
+        self.label.setText("")
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(parent=Form)
+        self.label_2.setGeometry(QtCore.QRect(340, 30, 331, 51))
+        font = QtGui.QFont()
+        font.setFamily("Sans Serif Collection")
+        font.setPointSize(18)
+        font.setBold(False)
+        font.setWeight(50)
+        self.label_2.setFont(font)
+        self.label_2.setObjectName("label_2")
+        self.pushButton = QtWidgets.QPushButton(parent=Form)
+        self.pushButton.setGeometry(QtCore.QRect(240, 100, 180, 90))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(parent=Form)
+        self.pushButton_2.setGeometry(QtCore.QRect(460, 100, 180, 90))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(parent=Form)
+        self.pushButton_3.setGeometry(QtCore.QRect(240, 230, 180, 90))
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_4 = QtWidgets.QPushButton(parent=Form)
+        self.pushButton_4.setGeometry(QtCore.QRect(460, 230, 180, 90))
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton_5 = QtWidgets.QPushButton(parent=Form)
+        self.pushButton_5.setGeometry(QtCore.QRect(350, 360, 180, 90))
+        self.pushButton_5.setObjectName("pushButton_5")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.label_2.setText(_translate("Form", "AREA ASSOCIATO"))
+        self.pushButton.setText(_translate("Form", "Visualizza storico gare"))
+        self.pushButton_2.setText(_translate("Form", "Visualizza gare"))
+        self.pushButton_3.setText(_translate("Form", "Accettazione gara"))
+        self.pushButton_4.setText(_translate("Form", "Scarica modulo referto"))
+        self.pushButton_5.setText(_translate("Form", "Carica modulo referto"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec())
